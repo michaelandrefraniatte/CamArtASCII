780b7180f29fc1f522e5a41781dbc7ad4e6739e8
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-07-27 10:12:05 +0200

    Initial commit

---
+++ Src/CamArtASCII/CamArtASCII/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace CamArtASCII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(679, 490);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 490);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CamArtASCII";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Src/CamArtASCII/CamArtASCII/Form1.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using System.Drawing.Imaging;
+using AForge;
+using AForge.Imaging;
+using System.Drawing.Drawing2D;
+namespace CamArtASCII
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        public static extern uint TimeBeginPeriod(uint ms);
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        public static extern uint TimeEndPeriod(uint ms);
+        [DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
+        public static extern void NtSetTimerResolution(uint DesiredResolution, bool SetResolution, ref uint CurrentResolution);
+        private FilterInfoCollection CaptureDevice;
+        private VideoCaptureDevice FinalFrame;
+        public static uint CurrentResolution = 0;
+        public static Form1 form = (Form1)Application.OpenForms["Form1"];
+        private static Bitmap img;
+        private VideoCapabilities[] videoCapabilities;
+        private string[] AsciiChars = { "#", "@", "$", "%", "&", "8", "?", "[", "]", "{", "}", "(", ")", "|", "+", ",", ":", "_", "-", "^", ".", "&nbsp;" };
+        private StringBuilder html = new StringBuilder();
+        private void Start()
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[0].MonikerString);
+            videoCapabilities = FinalFrame.VideoCapabilities;
+            FinalFrame.VideoResolution = videoCapabilities[videoCapabilities.Length - 1];
+            FinalFrame.NewFrame += FinalFrame_NewFrame;
+            FinalFrame.Start();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimeBeginPeriod(1);
+            NtSetTimerResolution(1, true, ref CurrentResolution);
+            Start();
+        }
+        private void task(Bitmap img)
+        {
+            Bitmap bmp = ResizeImage(img, 100, 35);
+            html.Clear();
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color col = bmp.GetPixel(x, y);
+                    int index = (int)((0.2126 * col.R + 0.7152 * col.G + 0.0722 * col.B) * (AsciiChars.Length - 1) / 255);
+                    html.Append(AsciiChars[index]);
+                }
+                html.Append("<br>");
+            }
+            webBrowser1.DocumentText = "<pre style=\"font-size: 10px;\">" + html.ToString() + "</pre>";
+        }
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            img = (Bitmap)eventArgs.Frame.Clone();
+            Task.Run(() => task(img));
+        }
+        public static Bitmap ResizeImage(System.Drawing.Image image, int width, int height)
+        {
+            return new Bitmap(image, width, height);
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.Threading.Thread.Sleep(1000);
+            if (FinalFrame.IsRunning == true)
+                FinalFrame.Stop();
+            TimeEndPeriod(1);
+        }
+    }
+}
